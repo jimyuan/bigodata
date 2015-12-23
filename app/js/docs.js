c69e72a0acc51f42df351fa255d9cee2ff841d70
Jim Yuan <aquarior@gmail.com>
2015-12-23 18:23:32 +0800

    master: 基本 UI 完成

--- app/js/docs.js
+++ app/js/docs.js
@@ -4,4 +4,23 @@
   function MBS() {
 
   }
+  var suggestOptions = {
+    allowFreeEntries: false,
+    maxEntryLength: null,
+    maxSelection: 10,
+    toggleOnClick: true,
+    useTabKey: true,
+    useCommaKey: true,
+    useZebraStyle: true,
+    placeholder: '点击选取或输入关键词',
+    noSuggestionText: '<i class="text-warning fa fa-exclamation-triangle"></i>'
+  }
+  $('#storeFront, #baseTarget, #compareEvent').magicSuggest($.extend(suggestOptions, {
+    data: ['Lorem ipsum dolor sit amet.','Voluptates impedit veniam aspernatur doloribus.','Voluptatem repellat eaque ad tempora!','Sit qui mollitia esse maxime.','Ea, unde, minima. Officiis, minus.','Perferendis incidunt harum saepe nihil.','Earum aut aliquid at vitae.','Officiis ipsa error fugit ea.','Enim consequatur, rerum laudantium incidunt.','Deleniti tempora a veritatis. Culpa.']
+  }));
+
+  $('#indexTable').on('click', 'thead input', function(e){
+    var i = $(this).attr('id').split('-')[1] - 0 + 1;
+    $('#indexTable td:nth-child(' + i + ')').toggleClass('active');
+  })
 })();
